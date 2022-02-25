d93df96247fd6cc3faa9909da5bcdaa79932b5c8
Vasily <vasily-bazhenov.03@mail.ru>
2022-02-25 15:32:30 +0300

    feat: add new sections

--- js/main.js
+++ js/main.js
@@ -1,9 +1,26 @@
 $(function () {
     
-    const swiper = new Swiper('.swiper', {
+    const swiper = new Swiper('.swiper-first', {
         // Optional parameters
         direction: 'horizontal',
         loop: true,
+        // Navigation arrows
+        navigation: {
+          nextEl: '.benefit__swiper__button-next',
+          prevEl: '.benefit__swiper__button-prev',
+        },
+      });
+
+      const swiper = new Swiper('.swiper-second', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+      
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      
         // Navigation arrows
         navigation: {
           nextEl: '.swiper-button-next',
